d47d8c39895c3e809fba9c61b49fa006e79170d9
rkotrys <robert.kotrys@gmail.com>
2020-09-08 21:11:13 +0200

    OK

--- showclock.py
+++ showclock.py
@@ -74,6 +74,8 @@ def main():
         if item[1] == 'f':
             clk.menu.add(item[0], partial( myexec, arg=item[2] ) )
 #            print( 'menu: ', item[2] )
+        if item[1] == 'p':
+            clk.menu.add(item[0], item[2] )
 
 #    clk.menu.add('INFO', pr0 )
 #    clk.menu.add('SET: dhcpcd', pr1 )
