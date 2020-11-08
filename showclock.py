c49736576783c346a71bed5f9a28fd1c4ee46d2d
rkotrys <robert.kotrys@gmail.com>
2020-11-08 13:40:21 +0100

    OK

--- showclock.py
+++ showclock.py
@@ -3,57 +3,10 @@ import subprocess as proc
 from functools import partial
 import Kbd2, Clock
 
-#def sinfo(name, state):
-#    print(u'Showinfo')
-
-
-#try:
-"""
-def menuload(filename):
-    menucontent = []
-    with open('menu.csv') as csvfile:
-        r = csv.reader( csvfile )
-        first = True
-        for row in r:
-            if first:
-                first = False
-                continue
-            if row[0][0] == '#':
-                continue
-            row[0] = row[0].replace(u'|',u'\n')
-            menucontent.append( row )
-    return menucontent if len(menucontent)>0 else False
-
-def myexec( label='', menu=False, arg=False ):
-    if arg==False:
-        return
-    a = []
-    for item in arg.split(' '):
-        if item != '':
-            a.append(item)
-    r = str(proc.check_output( a ), encoding='utf-8').strip()
-    print( label, r, '\n' )
-    menu.active = False
-#    return r
-
-def py_call( toexec=None, label="label", menu=None ):
-    exec( toexec )
-"""
-
 def main():
-    #m = menuload( 'menu.csv' )
     kbd = Kbd2.Kbd2()
     clk = Clock.clock(kbd)
     clk.menu.load("menu.csv")
-
-    """
-    for item in m:
-        if item[1] == 'f':
-            clk.menu.add(item[0], partial( myexec, arg=item[2] ) )
-#            print( 'menu: ', item[2] )
-        if item[1] == 'p':
-            clk.menu.add(item[0], partial( py_call, toexec=item[2] ) )
-    """
 
     kbd.sethanddle( 'k3', clk.nextbk )
     kbd.sethanddle( 'k2', clk.sinfo )
@@ -68,7 +21,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+    GPIO.cleanup()
 
-#except:
-#   print("except")
-#   GPIO.cleanup()
