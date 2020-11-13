53665b8cd4fb39a40bea2fa4c6d8d3ddb9aab75f
Robert Kotrys <robert.kotrys@gmail.com>
2020-11-13 12:16:15 +0100

    isonline

--- Clock.py
+++ Clock.py
@@ -37,7 +37,6 @@ class clock:
         self.showinfo = False
         self.btdev = {}
         self.kbd = kbd
-        self.isonline = False
         self.hostinfo = hlp.hostinfo()
         self.mmcinfo = hlp.getmmcinfo()
         self.s_color = tuple(self.cnf["clock"]["s_color"])
@@ -88,9 +87,7 @@ class clock:
         return Image.alpha_composite( hmim, im.rotate( -(360*t[2])/60, Image.BICUBIC ) )
 
     def isonline(self, ip='8.8.8.8'):
-        print( "IsOnLine 0 : {}\n".format(self.isonline_flag) )
         while self.go:
-            print( "IsOnLine 1 : {}\n".format(self.isonline_flag) )
             try:
                 r = str(proc.check_output(['/bin/ping', '-c', '3', '-i', '0.2', '-w', '1', '-q', ip] ), encoding='utf-8').strip()
             except proc.CalledProcessError:
