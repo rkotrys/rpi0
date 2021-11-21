0df48eabaaafd93c282f2f31bb142f16d35f4f7a
rkotrys <robert.kotrys@gmail.com>
2021-11-21 16:52:01 +0100

    drawnetwork

--- Clock.py
+++ Clock.py
@@ -161,7 +161,8 @@ class clock:
         if wififlag and ethflag:
             symbol = chr(clock.icons["wifi_eth"])+u''
         #draw.text( (128-17,1), symbol, font=self.symbols, fill=tuple(self.cnf["clock"]["icons_color"]) )
-        draw.text( (128-17,1), u'w', font=self.font12, fill=tuple(self.cnf["clock"]["icons_color"]) )
+        draw.text( (128-17,1), 
+                  'w', font=self.font12, fill=tuple(self.cnf["clock"]["icons_color"]) )
 
     def drawbt(self,draw):        
         if self.btscan_show:
