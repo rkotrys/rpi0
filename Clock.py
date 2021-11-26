10b01e3158683a510105d2fb74349091e76716e9
rkotrys <robert.kotrys@gmail.com>
2021-11-26 20:02:01 +0100

    iostat

--- Clock.py
+++ Clock.py
@@ -120,12 +120,13 @@ class clock:
             stats=json.loads(str(proc.check_output(['iostat','-o', 'JSON', '--dec=0', '1', '2']), encoding='utf-8') )
             cpu=stats['sysstat']['hosts'][0]["statistics"][1]["avg-cpu"]['idle']
             print(cpu)
-            r = []
-            lno=8
-            for l in cpulines[lno].split(' '):
-                if l == '': continue
-                r.append(int(l))
-            self.cpu = r[5]
+            #r = []
+            #lno=8
+            #for l in cpulines[lno].split(' '):
+            #    if l == '': continue
+            #    r.append(int(l))
+            self.cpu = int(cpu)
+            print(self.cpu)
             r = []
             for l in memlines[1].split(' '):
                 if l == '': continue
