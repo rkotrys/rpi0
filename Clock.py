612f26537f37e05a99da76afa3b6f54234e6fae1
rkotrys <robert.kotrys@gmail.com>
2021-12-03 08:38:59 +0100

    xxx

--- Clock.py
+++ Clock.py
@@ -106,7 +106,7 @@ class clock:
                     self.rpihub=True
                     # TODO: read respoce
                     r=json.loads(base64.standard_b64decode(x.text))
-                    #print( base64.standard_b64decode(x.text) )
+                    print( base64.standard_b64decode(x.text) )
                     if r['status']=='OK':
                         if not self.goodtime:
                             curent_date_time=str(r['time']).split()
