5509c73ee784a7af35e8f58d1aa0a32817c31ec1
rkotrys <robert.kotrys@gmail.com>
2022-01-16 16:47:16 +0100

    rplink outside

--- helper.py
+++ helper.py
@@ -1,188 +1,281 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
 
-import os, io, subprocess
-from pathlib import Path
+# /*****************************************************************************
+# * | File        :	  helper.py
+# * | Author      :   Robert Kotrys
+# * | Function    :   Basic functions to use with Raspberry Pi devives
+# * | Info        :
+# *----------------
+# * | This version:   V1.0
+# * | Date        :   2022-01-09
+# * | Info        :   
+# ******************************************************************************/
 
-def readfile(filename):
-    if os.path.isfile(filename):
-        with open(filename,'r') as file:
-            data = file.readline().strip().replace('\n','')
+import subprocess
+
+def getip():
+    """ get a first IP v.4 address """
+    ip="- no IP -"
+    for dev in ('eth0','wlan0'):
+        w=str( subprocess.run(["ip -4 a l "+dev+"|grep inet"], shell=True, capture_output=True, text=True ).stdout ).split()
+        if( len(w)>1 ):
+            ip = w[1]
+            break
+    return ip       
+
+def settime(address="rpi.ontime24.pl"):
+    """ get time from 'address' site and set the time to system clock """
+    try:
+        r = str(subprocess.check_output(['wget', '-O', '/tmp/datetime.txt', '-q', address+'/?get=datetime'] ), encoding='utf-8').strip()
+    except subprocess.CalledProcessError:
+        r= None
+    if r!=None:
+        with open("/tmp/datetime.txt","r") as f:
+            dt = f.read().split()
+    r = str(subprocess.check_output(['timedatectl', 'set-ntp', 'false'] ), encoding='utf-8').strip()    
+    r = str(subprocess.check_output(['timedatectl', 'set-time', dt[0] ]), encoding='utf-8').strip()
+    r = str(subprocess.check_output(['timedatectl', 'set-time', dt[1] ]), encoding='utf-8').strip()    
+
+def gettemp():
+    """ get the core temperature and return as float in 'C """
+    with open('/sys/class/thermal/thermal_zone0/temp', "r") as file:
+        tmp = float(file.read(5))/1000
+    return tmp
+
+def hostname(name=None):
+    """ get system hostname od set 'name' as system hostname"""
+    oldhostname=str( subprocess.run(["/bin/hostname"], capture_output=True, text=True ).stdout ).strip()
+    if name!=None:
+        # set hostname to name
+        subprocess.run(["/bin/hostname", name])
+        with open("/etc/hostname","w") as f:
+            f.write(name)
+        with open("/etc/hosts","r") as f:
+            oldhosts=str(f.read()).splitlines()
+        with open("/etc/hosts","w") as f:
+            for line in oldhosts:
+                if oldhostname in line:
+                    f.write(line.replace(oldhostname, name)+'\n')
+                else:     
+                    f.write(line+'\n')
     else:
-        data=False
+        # get hostname
+        hostname=str( subprocess.run(["/bin/hostname"], capture_output=True, text=True ).stdout ).strip()
+        return hostname
 
-    return data
-
-def system_exec(command):
-    return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode('utf-8').strip().split('\n')
-
-def hostinfo():
-    model = {}
-    with open('/proc/cpuinfo','r') as file:
-        for line in file:
-           data = line.strip().replace('\n','').split(':',2)
-           if  len(data)==2:
-               key=data[0].strip()
-               value=data[1].strip()
-               if not key in model:
-                   model[key] = data[1].strip()
-               elif key=="processor":
-                   model[key] = "{}".format( int(model[key])+1 )
-               if key=="Serial":
-                   model[key]=model[key][8:]
-        model['processor']="{}".format( int(model['processor'])+1 )
-
-    with open('/proc/meminfo','r') as file:
-        for line in file:
-           data = line.strip().replace('\n','').split(':',2)
-           if  len(data)==2:
-               key=data[0].strip()
-               value=data[1].strip()
-               if not key in model:
-                   model[key] = data[1].strip()
-
-    with open('/proc/version','r') as file:
-        data = file.readline().strip().split(' ', 4)
-        model['system'] = data[0] + ' ' + data[2]
-
-    dev = []
-    p=Path('/sys/class/net/');
-    for x in p.iterdir():
-        if x.is_dir():
-            l = str(x).split('/')
-            if l[4]=='lo': continue
-            with open(str(x)+'/address','r') as file:
-                data = file.readline().strip().replace('\n','')
-
-        dev.append( ( l[4], data ) )
-
-    model['dev']=dev
-    for line in system_exec('lsb_release -irc'):
-        data = line.split(':',2)
-        model[data[0].strip()]=data[1].strip()
-
-    return  model
-
-def getcputemp():
-    temperature='/sys/class/thermal/thermal_zone0/temp'
-    if os.path.isfile(temperature):
-        t=float(readfile('/sys/class/thermal/thermal_zone0/temp'))
-        t = t / 1000.0
+def find_net(buf):
+    start=str(buf).find('network={')
+    stop=str(buf).find('}')
+    if start>-1 and stop > start:
+        net=buf[start:(stop+1)]
     else:
-        t=0.0
-    return "{:4.1f}".format(t)
+        net=False
+    if net!=False:
+        ssid=net[net.find('ssid="')+len('ssid="'):]
+        ssid=ssid[0:ssid.find('"')]
+        return (net, ssid, buf[0:start]+buf[stop+1:])        
+    else:
+        return (False, '', buf)
 
-def getip(dev):
-    w=system_exec("ip -4 a l {}".format(dev) )
-    if len(w)<3:
-        return ""
-    w=w[1].strip().split(" ",2)
-    return w[1]
+def get_wlans_def():
+    with open('/etc/wpa_supplicant/wpa_supplicant.conf','rt') as f:
+        input=str(f.read()).strip()
+        (net_def, ssid_name, buf) = find_net(input)
+        if net_def==False:
+            return False
+    net_dic={}
+    net_dic[ssid_name]=net_def
+    while net_def!=False: 
+        (net_def, ssid_name, buf) = find_net(buf)
+        if net_def!=False:
+            net_dic[ssid_name]=net_def
+    return net_dic
 
-def getallip(dev):
-    w=system_exec("ip -4 a l {}".format(dev) )
-    if len(w)<3:
-        return ""
-    iplist=[]
-    for l in w:
-        if l.find("inet")>1:
-            iplist.append(l.strip().split(" ",2)[1])
+def get_wlans(sortkey='level'):
+    r = subprocess.run([ 'iwlist wlan0 scanning |grep -e Cell -e ESSID -e Quality -e  Channel:' ],shell=True,capture_output=True,encoding='utf-8')
+    wlans_dic={}
+    wlans=[]
+    if r.returncode==0:
+        #print(r.stdout)
+        lines=str(r.stdout).strip().splitlines()
+        i=0
+        while i<(len(lines)-3):
+            address = lines[i].strip().split()[4].strip()
+            channel = lines[i+1].strip().split(':')[1].strip()
+            level = lines[i+2].strip().split()[2].split('=')[1].strip()
+            name = lines[i+3].strip().split(':')[1].strip().replace('"', '')
+            wlans.append( {'address':address, 'channel':channel, 'level':level, 'name':name } )
+            i += 4
+        wlans.sort(reverse=True, key=lambda x:float(x[sortkey]))
+    for item in wlans: wlans_dic[item['name']]=item 
+    return wlans_dic
+
+def set_wpa_supplicant( essid, wpa_key, add=True, priority=1, country='pl' ):
+    if len(essid)>1 and len(wpa_key)>7:
+        head=u"country={}\nupdate_config=1\nctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\n".format(country)
+        net=str(u'\nnetwork={\nscan_ssid=1\nssid=\"[[1]]\"\npsk=[[2]]\npriority=[[3]]\n}\n')
+        r = subprocess.run(['echo '+str(wpa_key)+' |/bin/wpa_passphrase '+str(essid) ],shell=True,capture_output=True,encoding='utf-8')
+        if r.returncode==0:
+            psk=str(r.stdout).splitlines()[4].strip().split('=')[1]
+            net=net.replace( '[[1]]', essid ).replace( '[[2]]', psk) .replace( '[[3]]', str(priority) )
+            if add:
+                net_dic=get_wlans_def()
+                buf=head + net
+                for n in net_dic.keys():
+                    if n!=essid:
+                        buf=buf+'\n'+net_dic[n]
+            else:
+                buf = head + net
+            return( buf )
         else:
-            continue
-    return iplist
-
-def getmmcinfo():
-    w=system_exec("blkid -t TYPE=ext4 -o export")
-    r={}
-    for l in w:
-        _it=l.strip().split("=",2)
-        r[_it[0]] = _it[1]
-    w=system_exec("df -kH --type=ext4 --output=size,avail")
-    _tmp=w[1].strip()
-    r['size']=_tmp.split(" ")[0]
-    r['free']=_tmp[len(r['size']):].strip()
-    return r
-
-class RingBuffer:
-    def __init__(self,items):
-        self.data = items
-
-    def __str__(self):
-        r="[ "
-        for n in self.data:
-            r = r + "{} ".format(n)
-        return r + "]"
-
-    def get(self):
-        return self.data[0]
-
-    def next(self):
-        self.data.append(self.data.pop(0))
-        return self.data[0]
-
-    def add(self,item):
-        self.data.append(item)
-
-    def remove(self,item=None):
-        if item != None:
-            try:
-                self.data.remove(item)
-            except ValueError:
-                pass
+            return r.stdout+'\n'+r.stderr
+    else:
+        return "{} {}".format(essid, wpa_key)
+    
+def online_status(address="8.8.8.8"):
+    """ check on-line status od 'address' host with ping command """
+    try:
+        r = str(subprocess.check_output(['/bin/ping', '-4', '-c', '3', '-i', '0', '-f', '-q', address] ), encoding='utf-8').strip()
+    except subprocess.CalledProcessError:
+        r = '0 received'
+    ind = int(r.find(' received'))
+    if( int(r[ind-1:ind]) > 0 ):
+        return True
+    else:
+        return False        
+    
+def getnetdev():
+    """ scan net devices and return basic prams as dictionary: netdev[devname]=(devname,ip,mac) """
+    netdev={}
+    with open('/proc/net/dev','r') as f:
+        dev = f.read()
+        for devname in ['eth0', 'eth1', 'wlan0', 'wlan1']:
+            if dev.find(devname) > -1:
+                buf = str(subprocess.check_output([ 'ip', '-4', 'address', 'show', 'dev', devname ]), encoding='utf-8')
+                if len(buf)>1: 
+                    ip=buf.strip().splitlines()[1].split()[1]
+                else:
+                    ip='--'
+                mac=str(subprocess.check_output([ 'ip', 'link', 'show', 'dev', devname ]), encoding='utf-8').strip().splitlines()[1].split()[1]
+                netdev[devname]=(devname,ip,mac)
+    return netdev            
+    
+def getrpiinfo(dictionary=True, df={} ):
+    """ getrpiinfo(out=True) collect RPi params and status information, return as dictionary """
+    newdata = True if len(df)==0 else False
+    df['hostname']=str(subprocess.check_output(['hostname'] ), encoding='utf-8').strip()
+    netdev=getnetdev()
+    if "eth0" in netdev.keys():
+        ip=netdev['eth0'][1]
+        emac=netdev['eth0'][2]
+    else:
+        ip='--'
+        emac='--'
+    if "wlan0" in netdev.keys():
+        wip=netdev['wlan0'][1]
+        wmac=netdev['wlan0'][2]
+    else:
+        wip='--'
+        wmac='--'
+    df['ip']=ip
+    df['wip']=wip
+    df['emac']=emac
+    df['wmac']=wmac
+    with open('/proc/meminfo','r') as f:
+        df['memtotal']=int(str(f.readline()).strip().split()[1])//1000
+        df['memfree']=int(str(f.readline()).strip().split()[1])//1000
+        df['memavaiable']=int(str(f.readline()).strip().split()[1])//1000
+    r = subprocess.run(['iwgetid', '-r'], capture_output=True, encoding='utf-8')
+    ra = subprocess.run(['iwgetid', '-ra'], capture_output=True, encoding='utf-8')
+    rc = subprocess.run(['iwgetid', '-rc'], capture_output=True, encoding='utf-8')
+    if r.returncode==0:
+        df['essid']   = str(r.stdout).strip()
+        df['wlan_id'] = str(ra.stdout).strip()
+        df['wlan_ch'] = str(rc.stdout).strip()
+    else:
+        df['essid']   = '--'    
+        df['wlan_id'] = '--'
+        df['wlan_ch'] = '--'
+    df['coretemp']=gettemp()
+    # static
+    if newdata:
+        r = subprocess.run(['/bin/lscpu'], capture_output=True, encoding='utf-8')
+        if r.returncode==0:
+            lines=str(r.stdout).strip().splitlines()
+            df['cpus']=lines[2].split(':')[1].strip()
+            df['mdl']=lines[9].split(':')[1].strip()
+            df['maxfrq']=lines[11].split(':')[1].strip()
+            df['minfrq']=lines[12].split(':')[1].strip()
         else:
-            self.data.pop()
+            df['cpus']='--'
+            df['mdl']='--'
+            df['maxfrq']='--'
+            df['minfrq']='--'
+        with open('/boot/.id','r') as f:
+            df['msdid']=str(f.readline()).strip()
+        with open('/proc/cpuinfo','r') as f:
+            output=str(f.read()).strip().splitlines()
+        for line in output:
+            l=str(line).strip().split()
+            if len(l)>0 and l[0]=='Serial':
+                df['serial']=l[2][8:]
+            if len(l)>0 and l[0]=='Hardware':
+                df['chip']=l[2]
+            if len(l)>0 and l[0]=='Revision':
+                df['revision']=l[2]
+            if len(l)>0 and l[0]=='Model':
+                df['model']=str(u' '.join(l[2:])).replace('Raspberry Pi','RPi')
+        df['release']=str(subprocess.check_output(['uname','-r'] ), encoding='utf-8').strip()
+        df['version']='???'
+        with open('/etc/os-release','r') as f:
+            output=f.readlines()
+        for line in output:
+            l=line.split('=')
+            if l[0]!='VERSION':
+                continue
+            else:
+                df['version']=str(l[1]).strip().replace('"','').replace("\n",'') 
+                break   
+        df['machine']=str(subprocess.check_output(['uname','-m'] ), encoding='utf-8').strip()
+        buf=str(subprocess.check_output(['blkid','/dev/mmcblk0'] ), encoding='utf-8').strip().split()[1]
+        df['puuid']=buf[8:16]
+        buf=str(subprocess.check_output(['df','-h'] ), encoding='utf-8').strip().splitlines()[1].strip().split()
+        df['fs_total']=buf[1]
+        df['fs_free']=buf[3]
+        wlans=get_wlans_def()
+        wlans_str=u''
+        for w in wlans.keys(): wlans_str += u'{},'.format(w)
+        df['wlans']=wlans_str[0:len(wlans_str)-1] 
 
-class Wifiscan:
-    def __init__(self,dev="wlan0"):
-        self.dev = dev
-        self.res = system_exec("sudo iwlist {} scanning".format(dev))
-        self.data = self.scan()
-
-    def scan(self):
-        self.data = {}
-        f=list()
-        for line in self.res:
-            l = line.strip().replace('\n','')
-            if len(l)>0:
-                f.append( l )
-        for l in f:
-            n = f.index(l)
-            if l.find("Cell")>-1:
-                ofset=1
-                net={}
-                part = l.split(" ")
-                net["mac"] = part[4]
-                #data[part[1]] = net
-                while (n+ofset) < len(f)-1 and f[n+ofset].find("Cell")==-1 :
-                    if f[n+ofset].find("Channel")>-1:
-                        part = f[n+ofset].split(":")
-                        net["channel"]=part[1]
-                        ofset=ofset+1
-                    elif f[n+ofset].find("Frequency")>-1:
-                        part = f[n+ofset].split(" ")
-                        part = part[0].split(":")
-                        net["frequency"]=part[1]
-                        ofset=ofset+1
-                    elif f[n+ofset].find("Encryption")>-1:
-                        part = f[n+ofset].split(":")
-                        net["encryption"]=part[1]
-                        ofset=ofset+1
-                    elif f[n+ofset].find("ESSID")>-1:
-                        part = f[n+ofset].split(":")
-                        net["essid"]=part[1].strip('"')
-                        ofset=ofset+1
-                    elif f[n+ofset].find("Mode")>-1:
-                        part = f[n+ofset].split(":")
-                        net["mode"]=part[1]
-                        ofset=ofset+1
-                    elif f[n+ofset].find("Quality")>-1:
-                        part = f[n+ofset].split(" ")
-                        part2 = part[0].split("=")
-                        net["quality"]=part2[1]
-                        part2 = part[3].split("=")
-                        net["lavel"]=part2[1]
-                        ofset=ofset+1
-                    else:
-                        ofset=ofset+1
-                self.data[net["essid"]]=net
-        return self.data
+    if dictionary:
+        return df
+    else:
+        return rpiinfo_str( df )
+    
+def rpiinfo_str( df ):
+        buf=""
+        buf = buf + u"{}\n".format(df['model'])
+        buf = buf + u"host: {}\n".format(df['hostname'])
+        buf = buf + u"msdid: {}\n".format(df['msdid'])
+        buf = buf + u"ser: {}\n".format(df['serial'])
+        buf = buf + u"puuid: {}\n".format(df['puuid'])
+        buf = buf + u"HW:{} {}\n".format(df['chip'],df['machine'])
+        buf = buf + u"Linux: {}\n".format(df['release'])
+        buf = buf + u"ver: {}\n".format(df['version'])
+        buf = buf + u"ESSID: {}\n".format(df['essid'])
+        buf = buf + u"CPU temp: {:2.0f}\n".format(float(df['coretemp']))
+        if len(df['ip'])>2:
+            buf = buf + u"eth0 IP:\n {}\n".format(df['ip'])
+        if len(df['wip'])>2:
+            buf = buf + u"wlan0 IP:\n {}\n".format(df['wip'])
+        if len(df['emac'])>2:
+            buf = buf + u"eth0 MAC:\n {}\n".format(df['emac'])
+        if len(df['wmac'])>2:
+            buf = buf + u"wlan0 MAC:\n {}\n".format(df['wmac'])
+        for key, value in df.items():
+            if key in ['model', 'serial', 'hostname', 'chip', 'machine', 'puuid','release', 'version','essid','revision','msdid','coretemp','ip','wip','emac','wmac']:
+                continue
+            buf = buf + u"{}: {}\n".format(key,value)
+        return buf
+    
---
+++ rplink.py
@@ -0,0 +1,198 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+
+# /*****************************************************************************
+# * | File        :	  rplink.py
+# * | Author      :   Robert Kotrys
+# * | Function    :   Basic class to use with Raspberry Pi link service
+# * | Info        :   require files: helper.py
+# *----------------
+# * | This version:   V1.0
+# * | Date        :   2022-01-09
+# * | Info        :   include 'rplink' run file and 'rplink.service'
+# ******************************************************************************/
+
+
+import sys, time, sched, threading, requests, json, base64, logging, logging.handlers,signal,subprocess
+from logging.handlers import SysLogHandler
+from logging import Formatter
+from datetime import datetime
+import subprocess as proc
+import helper as h
+
+class rplink:
+    """ class 'rplink' xchange information and command with 'rpihub' server """
+    def __init__(self, display, rpilink_address='rpi.ontime24.pl',rpilink_period=1, localdata={}):
+        """ constructor """
+        self.display=display
+        self.rpilink_address=rpilink_address
+        self.rplink_period=rpilink_period
+        self.d=h.getrpiinfo()
+        self.n=h.getnetdev()
+        self.go=True
+        self.isonline=False
+        self.rpihub=False
+        self.goodtime=False
+        self.scan={}
+        self.logger = logging.getLogger(self.display)
+        self.logger.setLevel( logging.DEBUG )
+        self.log_handler = SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address='/dev/log')
+        self.log_handler.setFormatter(Formatter(fmt='[%(levelname)s] %(filename)s:%(funcName)s:%(lineno)d \"%(message)s\"'))
+        self.logger.addHandler( self.log_handler )
+        self.logger.info('[{}] rpilink logger is start!'.format(self.display))
+        self.localdata=localdata
+        proc.run(['/bin/timedatectl', 'set-ntp', 'false' ])
+        # start
+        self.x_checklink = threading.Thread( name='checklink', target=self.checklink, args=(), daemon=True)
+        self.x_rpilink = threading.Thread( name='rpilink', target=self.rpilink, args=(), daemon=True)
+        self.x_get_wlans = threading.Thread( name='get_wlans', target=self.get_wlans, args=(), daemon=True)
+        self.x_checklink.start()
+        self.x_rpilink.start()
+        self.x_get_wlans.start()
+
+    def setlocaldata(self,data):
+        for key, val in data.items():
+            self.localdata[key]=val
+
+    def getlocaldata(self):
+        return self.localdata
+    
+    def stop(self):
+        self.go=False
+
+    def checklink(self,address='8.8.8.8'):
+        """ thread """
+        while self.go:
+            self.isonline=h.online_status(address)
+            time.sleep(self.rplink_period)
+
+    def get_wlans(self):
+        """ thread """
+        while self.go:
+            time.sleep(1.5)
+            self.scan = h.get_wlans()
+            time.sleep(9.33)
+        
+    def rpilink(self):
+        """ thread """
+        while self.go:
+            time.sleep(self.rplink_period)    
+            if self.isonline:
+                self.d=h.getrpiinfo(self.d)
+                self.n=h.getnetdev()
+                self.setlocaldata( {'msdid':self.d['msdid'], 'essid':self.d['essid'], 'coretemp':self.d['coretemp'], 'memavaiable':self.d['memavaiable'],'cpus':self.d['cpus'],'maxfreq':self.d['maxfrq'],'minfreq':self.d['minfrq'],'wlans':self.d['wlans'],'wlan_id':self.d['wlan_id'],'wlan_ch':self.d['wlan_ch']} )
+                self.setlocaldata( {'scan':self.scan} )
+                #self.d['theme']= json.dumps({ 'display':self.display, 'localdata':self.localdata }) 
+                self.d['theme']=base64.standard_b64encode( bytes( json.dumps({ 'display':self.display, 'localdata':self.localdata }), 'utf-8' ) )
+                #df['theme']=self.cnf["global"]["theme"]
+                address_str = 'http://'+self.rpilink_address+'/?get=post'
+                try:
+                    x = requests.post( address_str, json=self.d, timeout=1)
+                except requests.exceptions.RequestException as e:
+                    self.logger.info( '[{}] post connection to {} fail'.format(self.display,self.rpilink_address) )
+                    continue
+                
+                #self.logger.debug( '[{}] post connection to {} has status code {}'.format(self.display,self.rpilink_address,x.status_code) )
+                if x.status_code==200:
+                    self.rpihub=True
+                    # read respoce
+                    r=json.loads(base64.standard_b64decode(x.text))
+                    #print( base64.standard_b64decode(x.text) )
+                    if r['status']=='OK':
+                        if not self.goodtime:
+                            now=datetime.now()
+                            date=now.strftime("%Y-%m-%d")
+                            curent_date_time=str(r['time']).split()
+                            if curent_date_time[0]!=date:
+                                proc.run(['/bin/timedatectl', 'set-time', curent_date_time[0] ])
+                            cp=proc.run(['/bin/timedatectl', 'set-time', curent_date_time[1] ])
+                            if cp.returncode==0:
+                                self.goodtime=True
+                        # set hostname    
+                        if r['cmd']['name']=='hostname' and r['cmd']['sn']==self.d['serial']:
+                            h.hostname(r['cmd']['value'])
+                            self.logger.debug( u'[{}] rplink_command: hostname is chneged from {} to {}'.format(self.display,self.d['hostname'],r['cmd']['value']) )
+                        # exec reboot
+                        if r['cmd']['name']=='reboot' and r['cmd']['sn']==self.d['serial']:
+                            self.logger.debug( u'[{}] rplink_command: system reboot'.format(self.display) )
+                            result = proc.run(['/bin/systemctl', 'reboot'],capture_output=True, text=True);
+                        # exec poweroff
+                        if r['cmd']['name']=='poweroff' and r['cmd']['sn']==self.d['serial']:
+                            self.logger.debug( u'[{}] rplink_command: system poweroff'.format(self.display) )
+                            result = proc.run(['/bin/systemctl', 'poweroff'],capture_output=True, text=True);
+                        # exec update agent software <LCD144,|oled13>
+                        if r['cmd']['name']=='update' and r['cmd']['sn']==self.d['serial']:
+                            self.logger.debug( u'[{}] rplink_command: code update from git repo'.format(self.display) )
+                            result = proc.run(['/bin/git pull'], cwd='/root/'+r['cmd']['service'], shell=True, capture_output=True, text=True);
+                            #print("stdout: ", result.stdout)
+                            #print("stderr: ", result.stderr)
+                        # set wpa_supplicand.conf data for a WLAN conection
+                        if r['cmd']['name']=='wlan_client' and r['cmd']['sn']==self.d['serial']:
+                            self.logger.debug( u'[{}] rplink_command: set wlan client connection data essid: {} wpa_key: {}'.format(self.display, r['cmd']['essid'], r['cmd']['wpa_key']) )
+                            out=h.set_wpa_supplicant(essid=r['cmd']['essid'],wpa_key=r['cmd']['wpa_key'])
+                            with open('/etc/wpa_supplicant/wpa_supplicant.conf', "wt") as f:
+                                f.write(out)
+                            run = subprocess.run(['/sbin/reboot','now'], capture_output=True, encoding='utf-8')    
+                            
+                    else:
+                        self.logger.debug( u'[{}] rplink_responce_error: {}'.format(self.display, r['status']) )
+                else:
+                    if self.rpihub==True:
+                        self.logger.debug( u'[{}] rplink_status_error: {}'.format(self.display, x.status_code ) )
+                    self.rpihub=False
+            else:
+                if self.rpihub==True:
+                    self.rpihub=False
+                    self.logger.debug( '[{}] device is OF-Line, address {}'.format(self.display,self.rpilink_address) )
+        else:
+            self.x_rpilink.stop() 
+            
+            
+                       
+# use the rplink 'solo' as a system service
+rpl=None
+def main():
+    global rpl
+    link_address=sys.argv[1] if len(sys.argv)>1 else 'rpi.ontime24.pl'
+    link_period=int(sys.argv[2]) if len(sys.argv)>2 else 1
+    local_data={ 'theme': 'headless' }
+    try:
+        signal.signal(signal.SIGINT, sigint_handler)
+        signal.signal(signal.SIGTERM, sigterm_handler)
+        signal.signal(signal.SIGHUP, sighup_handler)
+        rpl = rplink(display='solo',rpilink_address=link_address,rpilink_period=link_period, localdata=local_data)
+        
+        while rpl.go:
+            time.sleep(1)
+    
+    except IOError as e:
+        rpl.logger.debug( u'[{}] detect IOError {}'.format(rpl.display, e) )
+        print(e)
+        
+    except KeyboardInterrupt:    
+        rpl.logger.debug( u'[{}] exit by KeyboardInterrupt {}'.format(rpl.display) )
+        time.sleep(3)
+        sys.exit( 0 )    
+
+
+def sigint_handler(signum, frame):
+    global rpl
+    rpl.logger.debug( u'[{}] exit by sigint'.format(rpl.display) )
+    time.sleep(3)
+    sys.exit( 0 )    
+
+def sigterm_handler(signum, frame):
+    global rpl
+    rpl.logger.debug( u'[{}] exit by sigterm'.format(rpl.display) )
+    time.sleep(3) 
+    sys.exit( 0 )    
+
+def sighup_handler(signum, frame):
+    global rpl
+    rpl.logger.debug( u'[{}] get SIGHUP'.format(rpl.display) )
+
+
+if __name__ == "__main__":
+    main()
+
+#end of rpilink()
