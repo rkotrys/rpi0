0e7bd13a82bed1f5a53d2edcc5a1270fe9f78fcc
rkotrys <robert.kotrys@gmail.com>
2022-01-23 13:58:50 +0100

    xxx

--- helper.py
+++ helper.py
@@ -28,8 +28,10 @@ def addtextline(filename,textline):
     """ add a textline at the end of file with filename """
     with open("/root/.ssh/authorized_keys","rt") as f:
         lines = f.read().strip().splitlines()
-    lines.append(textline+"\n")  
-    print(lines)  
+    if isinstance(lines, list):
+        lines.append(textline+"\n")  
+    else:
+        lines=[].append(textline+"\n")    
     with open("/root/.ssh/authorized_keys","wt") as f:    
         f.write( "\n".join(lines) )
 
