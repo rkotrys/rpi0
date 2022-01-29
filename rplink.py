994654bd28ee99be16482d3099aecbc1b4465586
rkotrys <robert.kotrys@gmail.com>
2022-01-29 14:16:52 +0100

    xxx

--- rplink.py
+++ rplink.py
@@ -93,7 +93,7 @@ class rplink:
                 self.n=h.getnetdev()
                 self.AP=h.getapparam()
                 if self.AP!=False:
-                    pass
+                    self.AP['stations']=h.getap_stalist()
                 if self.clk!=None:
                     self.clk.df=self.d
                     self.clk.netdev=self.n
