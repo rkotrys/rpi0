b0ef2ebb09e1206040ebb0cccf0aa17bea14c63e
rkotrys <robert.kotrys@gmail.com>
2022-02-19 16:23:11 +0100

    xxx

--- rplink.py
+++ rplink.py
@@ -69,8 +69,11 @@ class rplink:
             
     def rfcommreset(self):
         r = str(subprocess.run([ 'ps -ef |grep -e pi |grep -e "/lib/systemd/systemd --user"'  ], shell=True, capture_output=True, text=True )).strip().split()
-        procno=r[1].strip()
-        subprocess.run([ 'kill -9 {}'.format(procno)  ], shell=True, capture_output=True, text=True )
+        while len(r)>0:
+            procno=r[1].strip()
+            subprocess.run([ 'kill -9 {}'.format(procno)  ], shell=True, capture_output=True, text=True )
+            r = str(subprocess.run([ 'ps -ef |grep -e "pi " |grep " 1 " '  ], shell=True, capture_output=True, text=True )).strip().split()
+                
             
     def stop(self):
         self.x_checklink.stop()
